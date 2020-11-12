13839ea5e16222f200211fb3bdcefe6a45eb2d29
Bartashevich Konstantin <kosbar28@gmail.com>
2020-11-13 01:11:57 +0500

    Рефакторинг класса Card, добавлен initialize constructor

--- Card.cpp
+++ Card.cpp
@@ -2,14 +2,14 @@
 
 #include "Card.h"
 bool Card::operator==(const Card &c) {
-    return (c.suit_of_card == suit_of_card) && (c.value_of_card == value_of_card);
+    return (c._suit_of_card == _suit_of_card) && (c._value_of_card == _value_of_card);
 }
 
 bool Card::operator>(const Card &c) {
-    return (c.suit_of_card == suit_of_card) && (c.value_of_card == value_of_card);
+    return (c._suit_of_card == _suit_of_card) && (c._value_of_card == _value_of_card);
 }
 
 std::ostream& operator<<(std::ostream &os, const Card &crd) {
-    os << crd.val << crd.sui;
+    os << crd._val << crd._sui;
     return os;
 }
--- Card.h
+++ Card.h
@@ -9,24 +9,28 @@
 #define CARD(X, Y) { _ ## X, #X, Y, #Y}
 //CARD(2, s) = {_2, 2, s, s} // struct card
 
+enum suit {
+    s = 2, h = 3, d = 5, c = 7
+};
+
+enum value {
+    _2 = 2, _3 = 3, _4 = 5, _5 = 7, _6 = 11, _7 = 13,
+    _8 = 17, _9 = 19, _T = 23, _J = 29, _Q = 31, _K = 37, _A = 41
+};
+
 class Card {
 private:
-    enum suit {
-        s = 2, h = 3, d = 5, c = 7
-    };
+    uint_fast8_t _value_of_card;
+    const char* _val; //для удобства вывода на печать дополнительные чары тут
 
-    enum value {
-        _2 = 2, _3 = 3, _4 = 5, _5 = 7, _6 = 11, _7 = 13,
-        _8 = 17, _9 = 19, _T = 23, _J = 29, _Q = 31, _K = 37, _A = 41
-    };
-
-    uint_fast8_t value_of_card;
-    const char *val; //для удобства вывода на печать дополнительные чары тут
-
-    uint_fast8_t suit_of_card;
-    const char *sui; //и тут
+    uint_fast8_t _suit_of_card;
+    const char* _sui; //и тут
 
 public:
+    Card(uint8_t value, char* cval, uint8_t suit, char* csui) :
+        _value_of_card(value), _val(cval),
+        _suit_of_card(suit), _sui(csui) {};
+
     bool operator==(const Card &c);
 
     bool operator>(const Card &c);
