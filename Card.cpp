7dc3d1ba2e0985f7be74a46cb29b121fc6547c7d
Bartashevich Konstantin <kosbar28@gmail.com>
2020-11-17 01:01:24 +0500

    Refactoring class Card: define заменён на switch

--- Card.cpp
+++ Card.cpp
@@ -1,15 +1,75 @@
 // Created by kosbar on 11/10/20.
 
 #include "Card.h"
+#include <string>
+#include <iostream>
+
+Card::Card(char const* c) {
+    Card((std::string) c);
+}
+
+Card::Card(std::string s) : _str(s) {
+    if (s.length() == 2) {
+        switch (s[0]) {
+            case '2' : _value_of_card = 2; break;
+            case '3' : _value_of_card = 3; break;
+            case '4' : _value_of_card = 5; break;
+            case '5' : _value_of_card = 7; break;
+            case '6' : _value_of_card = 11; break;
+            case '7' : _value_of_card = 13; break;
+            case '8' : _value_of_card = 17; break;
+            case '9' : _value_of_card = 19; break;
+            case 'T' : _value_of_card = 23; break;
+            case 'J' : _value_of_card = 29; break;
+            case 'Q' : _value_of_card = 31; break;
+            case 'K' : _value_of_card = 37; break;
+            case 'A' : _value_of_card = 41; break;
+            default :
+                std::cout << "Error uncorrected input";
+        }
+        switch (s[1]) {
+            case 's' : _suit_of_card = 2; break;
+            case 'h' : _suit_of_card = 3; break;
+            case 'd' : _suit_of_card = 5; break;
+            case 'c' : _suit_of_card = 7; break;
+            default :
+                std::cout << "Error uncorrected input";
+        }
+    }
+}
+
+Card& Card::operator=(const Card &c) {
+    this->_suit_of_card = c._suit_of_card;
+    this->_value_of_card = c._value_of_card;
+    this->_str = c._str;
+
+    return *this;
+}
+
 bool Card::operator==(const Card &c) {
     return (c._suit_of_card == _suit_of_card) && (c._value_of_card == _value_of_card);
 }
 
 bool Card::operator>(const Card &c) {
-    return (c._suit_of_card == _suit_of_card) && (c._value_of_card == _value_of_card);
+    return (c._value_of_card > _value_of_card);
+}
+
+bool Card::operator<(const Card &c) {
+    return (c._value_of_card < _value_of_card);
 }
 
 std::ostream& operator<<(std::ostream &os, const Card &crd) {
-    os << crd._val << crd._sui;
+    os << crd._str;
+
     return os;
+}
+
+std::istream &operator>>(std::istream &is, Card &c) {
+    std::string s;
+    is >> s;
+
+    Card tmp(s);
+    c = tmp;
+
+    return is;
 }
--- Card.h
+++ Card.h
@@ -6,36 +6,28 @@
 #include <cstdint>
 #include <ostream>
 
-#define CARD(X, Y) { _ ## X, #X, Y, #Y}
-//CARD(2, s) = {_2, 2, s, s} // struct card
-
-enum suit {
-    s = 2, h = 3, d = 5, c = 7
-};
-
-enum value {
-    _2 = 2, _3 = 3, _4 = 5, _5 = 7, _6 = 11, _7 = 13,
-    _8 = 17, _9 = 19, _T = 23, _J = 29, _Q = 31, _K = 37, _A = 41
-};
-
 class Card {
 private:
-    uint_fast8_t _value_of_card;
-    const char* _val; //для удобства вывода на печать дополнительные чары тут
-
-    uint_fast8_t _suit_of_card;
-    const char* _sui; //и тут
+    std::string _str = ""; //строка для ввода и вывода значения карты
+    uint_fast8_t _value_of_card = 0;
+    uint_fast8_t _suit_of_card = 0;
 
 public:
-    Card(uint8_t value, char* cval, uint8_t suit, char* csui) :
-        _value_of_card(value), _val(cval),
-        _suit_of_card(suit), _sui(csui) {};
+    Card(char const* c);
+
+    Card(std::string s);
+
+    Card& operator=(const Card& c);
 
     bool operator==(const Card &c);
 
     bool operator>(const Card &c);
 
+    bool operator<(const Card &c);
+
     friend std::ostream& operator<<(std::ostream &os, const Card &crd);
+
+    friend std::istream& operator>>(std::istream &is, Card& c);
 };
 
 #endif //POKERCALC_CARD_H
